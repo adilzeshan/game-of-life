6a0bc8dd9ea095dace9cf134f0099792c452b923
Adil Zeshan <adil@zeshan.org>
2019-04-26 15:37:27 +0100

    Tidy up games module

--- src/games.js
+++ src/games.js
@@ -2,104 +2,109 @@ import model from "./model";
 
 // GAMES
 
-const games = {
-  gameOfLife(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
+function gameOfLife(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
 
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount === 2 || neighbourCount === 3) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount === 2 || neighbourCount === 3) {
+      model.newGrid.makeCellLive(row, col);
     } else {
-      if (neighbourCount === 3) {
-        model.newGrid.makeCellLive(row, col);
-      }
+      model.newGrid.killCell(row, col);
     }
-  },
-
-  // Other games
-
-  maze(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
-
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount > 0 && neighbourCount < 6) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
-    } else {
-      if (neighbourCount === 3) {
-        model.newGrid.makeCellLive(row, col);
-      }
-    }
-  },
-
-  clouds(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
-
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount === 3 || neighbourCount === 4 || neighbourCount > 5) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
-    } else {
-      if (neighbourCount === 3 || neighbourCount > 5) {
-        model.newGrid.makeCellLive(row, col);
-      }
-    }
-  },
-
-  walledCity(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
-
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount > 3 && neighbourCount < 9) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
-    } else {
-      if (neighbourCount > 1 && neighbourCount < 6) {
-        model.newGrid.makeCellLive(row, col);
-      }
-    }
-  },
-
-  reverseGameOfLife(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
-
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount === 3) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
-    } else {
-      if (neighbourCount === 2 || neighbourCount === 3) {
-        model.newGrid.makeCellLive(row, col);
-      }
-    }
-  },
-
-  nightClub(row, col, neighbourCount) {
-    model.newGrid.killCell(row, col);
-
-    if (model.oldGrid.isCellLive(row, col)) {
-      if (neighbourCount < 3 || neighbourCount === 4) {
-        model.newGrid.makeCellLive(row, col);
-      } else {
-        model.newGrid.killCell(row, col);
-      }
-    } else {
-      if (neighbourCount < 4 || neighbourCount === 5) {
-        model.newGrid.makeCellLive(row, col);
-      }
+  } else {
+    if (neighbourCount === 3) {
+      model.newGrid.makeCellLive(row, col);
     }
   }
-};
+}
 
-export default games;
+// Other games
+
+function maze(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
+
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount > 0 && neighbourCount < 6) {
+      model.newGrid.makeCellLive(row, col);
+    } else {
+      model.newGrid.killCell(row, col);
+    }
+  } else {
+    if (neighbourCount === 3) {
+      model.newGrid.makeCellLive(row, col);
+    }
+  }
+}
+
+function nightOrDay(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
+
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount === 3 || neighbourCount === 4 || neighbourCount > 5) {
+      model.newGrid.makeCellLive(row, col);
+    } else {
+      model.newGrid.killCell(row, col);
+    }
+  } else {
+    if (neighbourCount === 3 || neighbourCount > 5) {
+      model.newGrid.makeCellLive(row, col);
+    }
+  }
+}
+
+function walledCity(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
+
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount > 3 && neighbourCount < 9) {
+      model.newGrid.makeCellLive(row, col);
+    } else {
+      model.newGrid.killCell(row, col);
+    }
+  } else {
+    if (neighbourCount > 1 && neighbourCount < 6) {
+      model.newGrid.makeCellLive(row, col);
+    }
+  }
+}
+
+function reverseGameOfLife(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
+
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount === 3) {
+      model.newGrid.makeCellLive(row, col);
+    } else {
+      model.newGrid.killCell(row, col);
+    }
+  } else {
+    if (neighbourCount === 2 || neighbourCount === 3) {
+      model.newGrid.makeCellLive(row, col);
+    }
+  }
+}
+
+function offAndOn(row, col, neighbourCount) {
+  model.newGrid.killCell(row, col);
+
+  if (model.oldGrid.isCellLive(row, col)) {
+    if (neighbourCount < 3 || neighbourCount === 4) {
+      model.newGrid.makeCellLive(row, col);
+    } else {
+      model.newGrid.killCell(row, col);
+    }
+  } else {
+    if (neighbourCount < 4 || neighbourCount === 5) {
+      model.newGrid.makeCellLive(row, col);
+    }
+  }
+}
+
+export default {
+  gameOfLife,
+  maze,
+  nightOrDay,
+  walledCity,
+  reverseGameOfLife,
+  offAndOn
+};
